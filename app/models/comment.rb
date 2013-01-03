1bf9939758d38fe83fddf198068b19ed00361716
Vasin Dmitriy <vasindima779@gmail.com>
2013-01-03 14:39:51 +0200

    Display a comment's state transition

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,10 +2,13 @@ class Comment < ActiveRecord::Base
   attr_accessible :text, :ticket_id, :user_id, :state_id
 
   after_create :set_ticket_state
+  before_create :set_previous_state
+
 
   belongs_to :user
   belongs_to :ticket
   belongs_to :state
+  belongs_to :previous_state, :class_name => "State"
 
   validates :text, :presence => true
 
@@ -17,4 +20,9 @@ class Comment < ActiveRecord::Base
       self.ticket.state = self.state
       self.ticket.save!
     end
+
+    def set_previous_state
+      self.previous_state = ticket.state
+    end
+
 end
